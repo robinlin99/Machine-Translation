e9313314a42b128b6d4d00a477d3294623e7b474
Robin Lin <robin.lin99@yahoo.com>
2021-02-04 14:34:36 -0500

    Update svm.py

--- svm.py
+++ svm.py
@@ -4,6 +4,8 @@ from nltk.tokenize import word_tokenize
 from sklearn import svm
 import matplotlib.pyplot as plt
 from sklearn.metrics import f1_score
+import seaborn as sns
+from sklearn import metrics
 
 def build_train(filename):
 	extracted = extract.extract(filename)
@@ -65,6 +67,18 @@ def predict(clf):
 		predicted.append(pred)
 	return predicted, test_data_y
 
+
+def predict_train_data(clf):
+	train_data_x, train_data_y = build_train("train.txt")
+	predicted = []
+	for sample in train_data_x:
+		proc_sample = np.array([list(sample)])
+		print(proc_sample)
+		pred = clf.predict(proc_sample)[0]
+		predicted.append(pred)
+	return predicted, train_data_y
+
+
 def accuracy(ground_truth, prediction):
 	total = len(ground_truth)
 	correct = 0
@@ -80,11 +94,31 @@ def classify():
 	clf = svm.SVC()
 	clf = svm_train(clf)
 	pred, ground_truth = predict(clf)
+	pred_train, ground_truth_train = predict_train_data(clf)
 	print(pred)
 	acc = accuracy(ground_truth,pred)
-	print("The % Accuracy is: " + str(float(acc*100)) + "%")
-	print("The F1 Score computed using Sklearn is: " + str(f1score(ground_truth, pred)))
-	
+	acc_train =  accuracy(ground_truth_train,pred_train)
+	print("The Test % Accuracy is: " + str(float(acc*100)) + "%")
+	print("The Test F1 Score computed using Sklearn is: " + str(f1score(ground_truth, pred)))
+	print("The Train % Accuracy is: " + str(float(acc_train*100)) + "%")
+	print("The Train F1 Score computed using Sklearn is: " + str(f1score(ground_truth_train, pred_train)))
+	cm = metrics.confusion_matrix(ground_truth, pred)
+	cm_train = metrics.confusion_matrix(ground_truth_train,pred_train)
+	print(cm)
+	print(cm_train)
+	plot_cm(acc, cm)
+	plot_cm(acc_train, cm_train)
+
+
+def plot_cm(acc, cm):
+	plt.figure(figsize=(9,9))
+	sns.heatmap(cm, annot=True, fmt=".3f", linewidths=.5, square = True, cmap = 'Blues_r')
+	plt.ylabel('Actual label')
+	plt.xlabel('Predicted label')
+	all_sample_title = 'Accuracy Score: {0}'.format(acc)
+	plt.title(all_sample_title, size = 15)
+	plt.show()
+
 
 classify()
 
